a392dc944bec4ec18ffdcd1b6b3b7b12770e72a7
harshita <harshita1225@gmail.com>
2022-07-05 12:34:33 +0200

    task completed

--- solution.js
+++ solution.js
@@ -1,1 +1,22 @@
 
+//q1
+
+//1.NAN stands for  Not a Number.
+
+const str1 = 'hello';
+console.log('q1.1 check if it is a NaN',str1===NaN);
+
+const str2 = '3';
+console.log('q1.2 check if it is a NaN',str2===NaN);
+
+console.log('q1.3', typeof NaN);// it is a number
+
+//q2
+console.log('q2.1', 0.1 * 0.2);
+const equation2= 0.1 * 0.2;
+console.log('q2.2', equation2.toFixed(2));
+
+//q3
+console.log('q3.1', Infinity/0);
+console.log('q3.2', Infinity/Infinity);
+console.log('q3.3', 1/0);
